0a624d2581b2bb22ff16ed220e6acd3b45d43a5d
complete_binary_tree <qaz41425@163.com>
2025-01-24 23:17:08 +0800

    做完了设置。

--- __init__.py
+++ __init__.py
@@ -6,11 +6,15 @@ import tkintertools.toolbox as toolbox
 import tkintertools.animation as animation
 import tkintertools.theme as theme
 
+import time
 import random
 import math
 import tools
 
 from WEinfo import *
+
+# 临时动画管理
+animations = []
 
 class App_main(tkt.Tk):
 	def __init__(self, *args, **kwargs) -> None:
@@ -26,42 +30,269 @@ class App_main(tkt.Tk):
 
 	# 销毁组件
 	def clear_window(self):
-		for widget in self.winfo_children():
-			widget.destroy()
+		global animations
+				
+		#controller = animation.generate(math.sin, 0, math.pi, map_y=False)
+		#animation.Animation(1000, self.alpha, controller = controller)
+
+		for at in animations:
+			at.stop()
+		animations.clear()
+		for cv in self.canvases:
+			cv.destroy()
 
 	def preload(self):
-		# 载入设置
-
 		# 载入语言
 		global lang
 		lang = tools.language()
 
+		# 载入设置
+		global _setting
+		_setting = settings()
+
 		# 载入 tips
 		global tip
-		tip = tools.tips()
+		tip = tools.tips(pack = _setting['tips_pack'] if 'tips_pack' in _setting else None)
 
 class Home():
-	def __init__(self, root):
+	def __init__(self, root : App_main):
 		global tip
 		global lang
+		global _setting
+		global animations
 
-		main_cv = tkt.Canvas(root, width = 540 / 3 * 2, expand = 'x', auto_zoom = True)
-		main_cv.place(x = (540-540/3*2)/2, y = 15)
+		root.clear_window()
 
-		tkt.Text(main_cv, (540/3, 0+54), fontsize = 32, text = f'WordEdit Alpha {WordEditVersion[0]}.{WordEditVersion[1]}', anchor = 'center', auto_update = True)
-		says = tkt.Text(main_cv, (540/3, 80), fontsize = 16, text = tip.random_tips(Language), anchor = 'center', auto_update = True)
-		tkt.Button(main_cv, (540/3-48*2-5, 54+54), fontsize = 24, text = lang.get('refresh', Language),size = [48*4+10,24+10], 
-			command = lambda: says.set(tip.random_tips(Language)))
-		tkt.Button(main_cv, (540/3-48*2-5, 54+54+54), fontsize = 24, text = lang.get('settings', Language),size = [48*4+10,24+10])
+		bg_cv = tkt.Canvas(root, width = 540, height = 607,auto_zoom = True)
+		bg_cv.place(x = 0, y = 0)
 
-		animation.GradientItem(
-            main_cv, says.texts[0].items[0], "fill", ("red", "orange"), 500, controller=lambda p: math.sin(p*math.pi), repeat=-1).start()
+		main_cv = tkt.Canvas(root, width = 540, expand = 'x', auto_zoom = True)
+		main_cv.place(x = 0, y = 15)
+
+		tkt.Text(main_cv, (540/2, 0+54), fontsize = 32, text = f'WordEdit Alpha {WordEditVersion[0]}.{WordEditVersion[1]}', anchor = 'center')
+		says = tkt.Text(main_cv, (540/2, 80), fontsize = 16, text = tip.random_tips(), anchor = 'center')
+		tkt.Button(main_cv, (540/2-48*2-5, 54+54), fontsize = 24, text = lang.get('tip_refresh', Language),size = [48*4+10,24+10], 
+			command = lambda: says.set(tip.random_tips()))
+		tkt.Button(main_cv, (540/2-48*2-5, 54+54+54), fontsize = 24, text = lang.get('settings', Language),
+			 size = [48*4+10,24+10], command = lambda: _setting.show(root))
+		tkt.Button(main_cv, (540/2-48*2-5, 54+54+54+54), fontsize = 24, text = lang.get('exit', Language),
+			 size = [48*4+10,24+10], command = lambda: root.quit())
+
+		an1 = animation.GradientItem(
+            main_cv, says.texts[0].items[0], "fill", (_setting['tips_color_begin'], _setting['tips_color_end']), _setting['tips_change_time'], controller=lambda p: math.sin(p*math.pi), repeat=-1)
+		animations.append(an1)
+		an1.start()
 		
 		main_cv.update_idletasks()
 		main_cv.zoom()
+
+class settings(tools.WEfile):
+	def __init__(self):
+		self.read('settings.WE')
+		self.update_settings()
+
+	#def __del__(self):
+	#	self.write('settings.WE')
+
+	def show(self, root : App_main, default = None):
+		root.clear_window() # 删掉！删掉！一定要删掉！
+		self.rt = root
+
+		cv = tkt.Canvas(root, width = 540, height = 607, expand = 'x', auto_zoom = True)
+		cv.place(x = 0, y = 0)
+
+		self.cvleft = cvleft = tkt.Canvas(root, width = 150, height = 607, auto_zoom = True)
+		cvleft.place(x = 0, y = 0)
+		self.create_left_rect(default)
+
+		self.cvright = cvright = tkt.Canvas(root, width = 540-150, height = 607, auto_zoom = True)
+		cvright.place(x = 150, y = 0)
+		
+	def create_left_rect(self, default = None):
+		if theme.get_color_mode() == 'light':
+			self.cvleft.create_rectangle(0, 0, 150, 607, fill = "gray97", outline = 'gray97')
+		else:
+			self.cvleft.create_rectangle(0, 0, 150, 607, fill = "gray3", outline = 'gray3')
+
+		tkt.Text(self.cvleft, (150 / 2, 32), text = lang.get('settings', Language), fontsize = 32, anchor = 'center')
+		tkt.SegmentedButton(self.cvleft, (150 / 2, 64), sizes = [[160, 40], [160, 40], [160, 40]], text = 
+					  (lang.get('show', Language), lang.get('tips', Language), lang.get('language', Language)), 
+					  fontsize = 24, anchor = 'n', layout="vertical", command = self.call_segbutton,default = default)
+		tkt.Button(self.cvleft, (150 / 2, 580), size = [160, 40], fontsize = 24,
+			 text = lang.get('return', Language), anchor = 'center', command = lambda root = root: Home(root))
+
+	# 各标签页的转换和渲染
+	def call_segbutton(self, index : int):
+		#print(index)
+		for ans in animations:
+			ans.stop()
+		self.cvright.destroy()
+		self.cvright = tkt.Canvas(root, width = 540-150, height = 607, auto_zoom = True)
+		self.cvright.place(x = 150, y = 0)
+		#for things in self.cvright.children.values():
+		#	print(type(things))
+		#	things.destroy()
+
+		match index:
+			case 0: # 图像
+				# ----dark mode----
+				tkt.Text(self.cvright, (10, 10), fontsize = 20, text = lang.get('dark_mode',Language))
+				def switchh():pass
+				sw = tkt.Switch(self.cvright, (300, 10), default = theme.get_color_mode() == 'dark', command = lambda b:switchh(b))
+				def switchh(b):
+					sw.disable()
+					theme.set_color_mode("dark" if b else "light")
+					self.create_left_rect()
+					sw.disable(False)
+				
+			case 1:
+				# ----小贴士包----
+				tkt.Text(self.cvright, (10, 10), fontsize = 20, text = lang.get('tips_pack', Language))
+
+				# ----小贴士包重新检测按钮----
+				entry = None
+				def reload_command():
+					tip.load()
+					self.call_segbutton(1)
+
+				tkt.Button(self.cvright, (170, 48), fontsize = 20, text = lang.get('refresh_tp', Language),
+			   			   size = [200, 30], command = reload_command)
+				
+				# 颜色选择
+				tkt.Text(self.cvright, (10, 90), fontsize = 20, text = lang.get('tips_change_color', Language))
+				txt = tkt.Button(self.cvright, (150, 88), fontsize = 20, text = _setting['tips_color_begin'], 
+					 size = [100, 30], command = lambda: tkt.TkColorChooser(title = lang.get('select_tips_color', Language), 
+											 color = _setting['tips_color_begin'], master = self.rt, command = select_color_1))
+				an1 = animation.GradientItem(
+            		self.cvright, txt.texts[0].items[0], "fill", (_setting['tips_color_begin'], _setting['tips_color_begin']), 1, repeat = -1)
+				an1.start()
+				txt2 = tkt.Button(self.cvright, (270, 88), fontsize = 20, text = _setting['tips_color_end'], 
+					  size = [100, 30], command = lambda: tkt.TkColorChooser(title = lang.get('select_tips_color', Language), 
+											 color = _setting['tips_color_end'], master = self.rt, command = select_color_2))
+				an2 = animation.GradientItem(
+            		self.cvright, txt2.texts[0].items[0], "fill", (_setting['tips_color_end'], _setting['tips_color_end']), 1, repeat = -1)
+				an2.start()
+				animations.append(an1)
+				animations.append(an2)
+				tkt.Text(self.cvright, (255, 90), fontsize = 20, text = '~')
+
+				def select_color_1(color):
+					_setting['tips_color_begin'] = color
+					self.call_segbutton(1)
+				def select_color_2(color):
+					_setting['tips_color_end'] = color
+					self.call_segbutton(1)
+
+				# 时间修改
+				def change_command(new_val):
+					if self.rt.focus_get() == entry_sb: return
+					new_val = entry_sb.get()
+					print('new_val')
+					try:
+						new_val = int(new_val)
+					except ValueError:
+						print('VE')
+						messagebox.showerror(message = lang.get('time_should_be_int', Language))
+						#if entry_sb != None: entry_sb.set(str(_setting['tips_change_time']))
+						self.call_segbutton(1)
+						return
+					
+					if new_val <= 0:
+						print('VE2')
+						messagebox.showerror(message = lang.get('time_should_bigger_than_0', Language))
+						#if entry_sb != None: entry_sb.set(str(_setting['tips_change_time']))
+						self.call_segbutton(1)
+						return
+					
+					_setting['tips_change_time'] = new_val
+
+				tkt.Text(self.cvright, (10, 130), fontsize = 20, text = lang.get('tips_change_time', Language))
+				#tmp_cv = tkt.Canvas(self.cvright, width = 120, height = 50, auto_zoom = True).place(x = 190, y = 118)
+				entry_sb = tkt.SpinBox(self.cvright, (200, 128), fontsize = 20, default = str(_setting['tips_change_time']), size = [100, 30])
+				entry_sb.bind('<Leave>', change_command)
+
+				# ----小贴士包切换按钮----
+				def sure_command(b : int):pass
+				entry = tkt.OptionButton(self.cvright, (170, 8), fontsize = 20, text = tip.packs, 
+							 size = [200,30], align = 'down',command = lambda b: sure_command(b))
+				entry.set(tip.packs.index(tip.pack))				
+
+				def sure_command(b : int):
+					global Language
+					#print(entry.get())
+					tip.pack = _setting['tips_pack'] = tip.packs[b]
+				
+				# ----小贴士提示----
+				tkt.Text(self.cvright, (10, 170), fontsize = 20, text = lang.get('tips_pack_tip', Language))
+
+			case 2:
+				# ----文本----
+				# ----语言切换----
+				tkt.Text(self.cvright, (10, 10), fontsize = 20, text = lang.get('language',Language))
+
+				#tkt.Button(self.cvright, (355, 8), fontsize = 20, size = [30, 30], text = '√', family = 'Consolas', command = sure_command)
+
+				# ----语言切换提示----
+				tkt.Text(self.cvright, (10, 90), fontsize = 20, text = lang.get('language_change_tip',Language))
+
+				# ----按钮----
+				# 刷新语言按钮
+				tkt.Button(self.cvright, (170, 48), fontsize = 20, text = lang.get('refresh_l', Language), 
+							 size = [200,30], command = lambda: 0 or lang.__init__() or self.call_segbutton(2))
+				# 语言切换按钮
+				def sure_command(b : int):pass
+				entry = tkt.OptionButton(self.cvright, (170, 8), fontsize = 20, text = list(lang.keys()), 
+							 size = [200,30], align = 'down',command = lambda b: sure_command(b))
+				entry.set(list(lang.keys()).index(Language))
+
+				def sure_command(b : int):
+					global Language
+					#print(entry.get())
+					_setting['language'] = Language = list(lang.keys())[b]
+					self.show(self.rt, 2)
+					self.call_segbutton(2)
+
+
+	def update_settings(self):
+		global Language
+
+		#print(self)
+
+		if 'language' in self.keys():
+			Language = self['language']
+			if not (Language in lang.keys()):
+				print('settings:language cannot find.')
+				Language = self['language'] = 'English'
+		else:
+			Language = 'English'
+
+		if 'tips_pack' in self.keys():
+			pass
+		else:
+			self['tips_pack'] = 'English'
+		if 'dark_mode' in self.keys():
+			theme.set_color_mode('dark' if self['dark_mode'] else 'light')
+
+		if not ('tips_color_begin' in self.keys() and 'tips_color_end' in self.keys()):
+			self['tips_color_begin'] = '#FFA500'
+			self['tips_color_end'] = '#FF0000'
+
+		if not 'tips_change_time' in self.keys():
+			self['tips_change_time'] = 500
+
+	def save(self):
+		self['dark_mode'] = theme.get_color_mode() == 'dark'
+		self.write('settings.WE')
 
 # 设置字体
 if toolbox.load_font("./LXGWWenKai-Regular.ttf"):
     configs.Font.family = "LXGW WenKai"
 root = App_main(title = f'WordEdit Alpha {WordEditVersion[0]}.{WordEditVersion[1]}',size=[540, 607])
 root.mainloop()
+
+# 保存设置
+_setting.save()
+
+# 删除动画
+for a in animations:
+	a.stop()
--- tools.py
+++ tools.py
@@ -1,19 +1,34 @@
 ﻿import random
 import os
+import tkintertools as tkt
 
 # tips~
 class tips(dict):
-	def __init__(self):
-		self.languages = []
+	def __init__(self, pack):
+		self.pack = pack
+		
+		self.load()
+		
+		if not (self.pack in self.packs):
+			print(f'[Warning]TipsLoader:tippack "{self.pack}" is not exist.')
+			self.pack = 'English(Inside)'
+
+	def load(self):
+		self.packs = ['English(Inside)']
+		self['English(Inside)'] = ['Oh there is no tips!', 'Try to click "Refresh"!']
+
 		files = [f for f in os.listdir('./tips') if os.path.isfile(os.path.join('./tips', f))] # 获取文件夹下所有语言文件
 		for file in files:
 			if file[-4:] == '.txt':
-				self.languages.append(file[:-4])
-				print(f'tips:find new language {file[:-4]}')
+				self.packs.append(file[:-4])
 				self[file[:-4]] = [line if line[-1] != '\n' else line[:-1] for line in open('./tips/'+file, 'r', encoding = 'UTF-8')]
+		print(f'TipsLoader:Find {len(self.keys())} tipspacks.')
 
-	def random_tips(self,language = 'Chinese'):
-		return self[language][random.randint(0,len(self[language])-1)]
+	def random_tips(self):
+		if self.pack in self.packs:
+			return self[self.pack][random.randint(0,len(self[self.pack])-1)]
+		else:
+			return ' '
 	
 # WE file（类 JS）
 class WEfile(dict):
@@ -38,6 +53,17 @@ class WEfile(dict):
 
 						if val[0] == val[-1] and (val[0] == '"' or val[-1] == "'"):
 							val = val[1:-1]
+							i = 0
+							while i < len(val):
+								if val[i] == '\\':
+									if val[i+1] == 'n':
+										val = val[:i] + '\n' + val[i+2:]
+									elif val[i+1] == '\\':
+										val = val[:i] + '\\' + val[i+2:]
+									else:
+										print('[Warning]WEreader:Invalid character after \'\\\'.')
+								i += 1
+
 						elif val[-1] == 'f':
 							val = float(val[:-1])
 						else:
@@ -47,7 +73,11 @@ class WEfile(dict):
 						#print(key,val)
 
 					except ValueError:
-						print(f'[ERROR]WEreader:The format of {filename} is incorrect.')
+						print(f'[ERROR]WEreader:The format of {filename} is incorrect.(ValueError)')
+						return 2
+					
+					except IndexError:
+						print(f'[ERROR]WEreader:The format of {filename} is incorrect.(IndexError)')
 						return 2
 					
 		except FileNotFoundError:
@@ -68,6 +98,9 @@ class WEfile(dict):
 		try:
 			with open(filename, 'w', encoding = 'UTF-8') as writer:
 				for key, value in self.items():
+					if type(value) == bool:
+						value = int(value)
+
 					if type(value) == int:
 						value = str(value)
 					elif type(value) == float:
@@ -108,17 +141,58 @@ class language(dict):
 
 	def load_english(self):
 		self['English'] = {
-			'refresh' : 'Refresh',
+			'tip_refresh' : 'Refresh tips',
 			'settings' : 'Settings',
 			'start' : 'Start',
 			'none' : 'No Text',
-			'test' : 'This is an English text.'
+			'test' : 'This is an English text.',
+			'show' : 'Display',
+			'dark_mode' : 'Dark mode',
+			'exit' : 'Exit',
+			'return' : 'Return',
+			'language' : 'Language',
+			'tips' : 'Tips',
+			'language_change_tip' : 'More language?\nPut the language file in "./Language" \nfolder,then click "Refresh language list".',
+			'refresh_l' : 'Refresh language list',
+			'tips_pack' : 'Tip pack',
+			'refresh_tp' : 'Refresh packs',
+			'tips_change_color' : 'Flick color',
+			'tips_change_time' : 'Flick time(ms)',
+			'tips_pack_tip' : 'More tip packs?\nPut the tip pack file in "./tips" \nfolder,then click "Refresh packs".'
 		}
 
 	def get(self, _key = 'none', language = 'English'):
 		if _key in self[language].keys():
 			return self[language][_key]
-		return self['English'][_key]
+		print(f'language:cannot find word "{_key}" in language "{language}".')
+		if _key in self['English'].keys():
+			return self['English'][_key]
+		else:
+			return _key
+
+# deepseek贡献的BetterSpinbox，具有焦点离开时回调功能
+class CustomSpinBox(tkt.SpinBox):
+	def __init__(self, root, pos, **kwrgs):
+		if 'command_fc_out' in kwrgs.keys():
+			print(kwrgs['command_fc_out'])
+			self.command_fc_out = kwrgs['command_fc_out']
+			del kwrgs['command_fc_out']
+		super().__init__(root, pos, **kwrgs)
+		self.clicked = False  # 记录是否点击了 SpinBox
+		self.bind('<Button>', self.on_mouse_clicked)
+
+	def on_mouse_clicked(self, x, y, button):
+		"""处理鼠标点击事件"""
+		if self.collide_point(x, y):  # 判断是否点击了 SpinBox
+			self.clicked = True
+		elif self.clicked and not self.collide_point(x, y):  # 判断是否离开了 SpinBox
+			if self.command_fc_out:  # 如果提供了回调函数
+				self.command_fc_out(self.get_value())  # 调用回调函数
+			self.clicked = False
+
+	#def on_mouse_released(self, x, y, button):
+	#	"""处理鼠标释放事件"""
+		
 
 """
 # 玩 florr 玩的
@@ -151,7 +225,7 @@ if __name__ == '__main__':
 	# ----language----
 	testlang = language()
 	print(testlang['English'])
-	print(testlang['Chinese'])
-	print(testlang.get(language = 'Chinese'))
+	print(testlang['中文（简体）'])
+	print(testlang.get(language = '中文（简体）'))
 	print(testlang.get('test'))
-	print(testlang.get('test', 'Chinese'))
+	print(testlang.get('test', '中文（简体）'))
